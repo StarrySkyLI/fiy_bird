4c1189c7b8e3ad4212ace0936169245af7622c40
starry <1515770217@qq.com>
2024-10-11 14:53:44 +0800

    feat: v0.0.2 add state

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,74 @@
 use bracket_lib::prelude::*;
-
+//模式
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 //状态
 struct State {
-
+    mode: GameMode,
 }
-impl GameState for State {
-    fn tick(&mut self,ctx : &mut BTerm){
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1,1,"hello,bracket terminal!");
+        ctx.print_centered(5, "Welcome to Fly Bird");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "you are dead");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 }
-fn main() -> BError{
-    let context =BTermBuilder::simple80x50()
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => {
+                self.main_menu(ctx);
+            }
+            GameMode::Playing => {
+                self.dead(ctx);
+            }
+            GameMode::End => {
+                self.play(ctx);
+            }
+        }
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
         .with_title("Fly Bird")
         .build()?;
-    main_loop(context,State{})
+    main_loop(context, State::new())
 }
